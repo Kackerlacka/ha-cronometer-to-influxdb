1eae46201698f8920f67e63fcb3f709c24d22065
Kackerlacka <43557714+Kackerlacka@users.noreply.github.com>
2024-06-03 16:55:33 -0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Get the environment variables
+CRONOMETER_USERNAME=${CRONOMETER_USERNAME}
+CRONOMETER_PASSWORD=${CRONOMETER_PASSWORD}
+INFLUXDB_USERNAME=${INFLUXDB_USERNAME}
+INFLUXDB_PASSWORD=${INFLUXDB_PASSWORD}
+
+# Run your Go application
+./go-app -cronometer_username $CRONOMETER_USERNAME -cronometer_password $CRONOMETER_PASSWORD -influxdb_username $INFLUXDB_USERNAME -influxdb_password $INFLUXDB_PASSWORD
