1f0327826a2aac8ad1b9347fadc9ab0486267ee1
Kackerlacka <43557714+Kackerlacka@users.noreply.github.com>
2024-06-03 17:23:41 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # Start the cron service
 service cron start
 
+# Load bashio functions
+source /usr/lib/hassio-addons/bashio
+
 # Get the environment variables
-CRONOMETER_USERNAME=${CRONOMETER_USERNAME}
-CRONOMETER_PASSWORD=${CRONOMETER_PASSWORD}
-INFLUXDB_USERNAME=${INFLUXDB_USERNAME}
-INFLUXDB_PASSWORD=${INFLUXDB_PASSWORD}
+CRONOMETER_USERNAME=$(bashio::config 'cronometer_username')
+CRONOMETER_PASSWORD=$(bashio::config 'cronometer_password')
+INFLUXDB_USERNAME=$(bashio::config 'influxdb_username')
+INFLUXDB_PASSWORD=$(bashio::config 'influxdb_password')
 
 # Run your Go application
-./go-app -cronometer_username $CRONOMETER_USERNAME -cronometer_password $CRONOMETER_PASSWORD -influxdb_username $INFLUXDB_USERNAME -influxdb_password $INFLUXDB_PASSWORD
+./go-app -cronometer_username "$CRONOMETER_USERNAME" -cronometer_password "$CRONOMETER_PASSWORD" -influxdb_username "$INFLUXDB_USERNAME" -influxdb_password "$INFLUXDB_PASSWORD"
