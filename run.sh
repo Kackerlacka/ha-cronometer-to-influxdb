cbdbf56790b4baae8db2a28d23058429e1becc0f
Kackerlacka <43557714+Kackerlacka@users.noreply.github.com>
2024-06-03 17:56:43 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,9 +6,10 @@ service cron start
 # Get the environment variables
 CRONOMETER_USERNAME=$CRONOMETER_USERNAME
 CRONOMETER_PASSWORD=$CRONOMETER_PASSWORD
+INFLUXDB_URL=$INFLUXDB_URL
 INFLUXDB_USERNAME=$INFLUXDB_USERNAME
 INFLUXDB_PASSWORD=$INFLUXDB_PASSWORD
-INFLUXDB_DATABASE=$INFLUXDB_DATABASE # New variable for InfluxDB database
+INFLUXDB_DATABASE=$INFLUXDB_DATABASE
 
 # Run your Go application
-./go-app -cronometer_username "$CRONOMETER_USERNAME" -cronometer_password "$CRONOMETER_PASSWORD" -influxdb_username "$INFLUXDB_USERNAME" -influxdb_password "$INFLUXDB_PASSWORD" -influxdb_database "$INFLUXDB_DATABASE"
+./go-app -cronometer_username "$CRONOMETER_USERNAME" -cronometer_password "$CRONOMETER_PASSWORD" -influxdb_url "$INFLUXDB_URL" -influxdb_username "$INFLUXDB_USERNAME" -influxdb_password "$INFLUXDB_PASSWORD" -influxdb_database "$INFLUXDB_DATABASE"
