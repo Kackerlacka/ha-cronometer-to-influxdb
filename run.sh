ada2737a4d555cc6e3bd6d8bc55d6f962f637aa9
Kackerlacka <43557714+Kackerlacka@users.noreply.github.com>
2024-06-04 23:55:03 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,13 @@ crond
 # List the contents of the directory
 ls -l
 
+# Print environment variables to verify they are set
+echo "CRONOMETER_USERNAME: $CRONOMETER_USERNAME"
+echo "CRONOMETER_PASSWORD: $CRONOMETER_PASSWORD"
+echo "INFLUXDB_URL: $INFLUXDB_URL"
+echo "INFLUXDB_USERNAME: $INFLUXDB_USERNAME"
+echo "INFLUXDB_PASSWORD: $INFLUXDB_PASSWORD"
+echo "INFLUXDB_DATABASE: $INFLUXDB_DATABASE"
+
 # Run your Go application
 ./cronapp -cronometer_username "$CRONOMETER_USERNAME" -cronometer_password "$CRONOMETER_PASSWORD" -influxdb_url "$INFLUXDB_URL" -influxdb_username "$INFLUXDB_USERNAME" -influxdb_password "$INFLUXDB_PASSWORD" -influxdb_database "$INFLUXDB_DATABASE"
